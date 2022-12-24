1133e78ad85838d1b0678f8f64773b2599200eb0
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 04:00:59 +0100

    commit zero

---
+++ Jailbreak Script pastebin roblox Jailbreak Auto-Rob .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:GetObjects("rbxassetid://1461971147")[1].Source)()  local vu = game:GetService("VirtualUser") game:GetService("Players").LocalPlayer.Idled:connect(function()  vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)  wait(1)  vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame) end)
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Auto Money Farmer for Jailbreak, fully automatic money farmer script that will rob all places.
+						
+]]
